965b7445481e18da3121313d7834c8282f2ba770
Mr Krux <krux@krux.fr>
2024-08-24 22:03:46 +0200

    more tests and cleanup

--- context/logger/logger_test.go
+++ context/logger/logger_test.go
@@ -3,15 +3,26 @@ package logger
 import (
 	"context"
 	"testing"
+
+	"github.com/sirupsen/logrus"
 )
 
 func TestTTLogLevels(t *testing.T) {
 	// Test all log levels
 	logger := NewLogger(context.TODO(), "test")
+
+	name := logger.GetName()
+	if name != "test" {
+		t.Fatalf("Expected 'test', got '%s'", name)
+	}
+
+	logger.SetLevel(logrus.WarnLevel)
+
 	logger.TraceF("trace")
 	logger.DebugF("debug")
 	logger.InfoF("info")
 	logger.WarnF("warn")
+	logger.ErrorF("error")
 	defer func() {
 		if r := recover(); r == nil {
 			t.Fatalf("Expected panic")
@@ -20,5 +31,17 @@ func TestTTLogLevels(t *testing.T) {
 	logger.PanicF("panic")
 
 	//recover panic
+
+}
+
+func TestTTSubLogger(t *testing.T) {
+	// Test sublogger
+	logger := NewLogger(context.TODO(), "test")
+	sublogger := logger.CreateSubLogger("sub", logger)
+
+	name := sublogger.GetName()
+	if name != "test.sub" {
+		t.Fatalf("Expected 'test.sub', got '%s'", name)
+	}
 
 }
