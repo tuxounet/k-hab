742624c0972fc7552f28a6293c0b49fb904f2050
Mr Krux <krux@krux.fr>
2024-08-25 16:25:38 +0200

    review default config with Setup

--- bases/Setup.go
+++ bases/Setup.go
@@ -9,7 +9,6 @@ type SetupContainer struct {
 	Name        string                 `yaml:"name"`
 	Base        string                 `yaml:"base"`
 	Shell       string                 `yaml:"shell"`
-	Entry       string                 `yaml:"entry"`
 	Network     map[string]interface{} `yaml:"network"`
 	Proxy       map[string]interface{} `yaml:"proxy"`
 	Provision   string                 `yaml:"provision"`
@@ -21,7 +20,6 @@ func (hcc *SetupContainer) ToMap() map[string]interface{} {
 		"name":        hcc.Name,
 		"base":        hcc.Base,
 		"shell":       hcc.Shell,
-		"entry":       hcc.Entry,
 		"network":     hcc.Network,
 		"proxy":       hcc.Proxy,
 		"provision":   hcc.Provision,
